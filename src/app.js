d7a4575594fb980bf28f7d8ab9ab86f07c0413c9
leppekj <leppekj@uchicago.edu>
2021-03-18 08:14:15 -0500

    again

--- src/app.js
+++ src/app.js
@@ -86,7 +86,7 @@ var svg = svgContainer
 
     // svg.call(zoom);
 
-  csv('https://github.com/leppekja/fictional-octo-guide/tree/main/data')
+  csv('https://github.com/leppekja/fictional-octo-guide/blob/main/data/TestGeocodeToJson.csv')
     .then(points => {
       
       const points_margin ={top: -40, bottom: 0, right: 0, left: 100};
